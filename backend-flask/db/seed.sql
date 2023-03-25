22acd4490b2a8552e579981749884ca948c83309
Edgar Rodrigues <acidlangar@yahoo.com>
2023-03-25 12:01:59 +0000

    create new message and new message group

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Edgar Rodrigues', 'acidlangar@yahoo.com', 'erodrigues' ,'MOCK'),
-  ('Pedro Perez', 'earodriguez@gmail.com', 'pperez' ,'MOCK');
+  ('Pedro Perez', 'earodriguez@gmail.com', 'pperez' ,'MOCK'),
+  ('Londo Mollari', 'lmollari@centari.com', 'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
